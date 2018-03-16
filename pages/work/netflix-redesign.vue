701485ee42e95b8474c0e7ea330f8f5af9f8d7b2
Aleks <sasko_derp@me.com>
2018-03-16 21:56:41 +0200

    Update netflix-redesign.vue

--- pages/work/netflix-redesign.vue
+++ pages/work/netflix-redesign.vue
@@ -13,25 +13,25 @@
 
     <div class="uk-grid uk-child-width-1-3 uk-margin-large-bottom">
       <div>
-        <img src="https://via.placeholder.com/400">
-        <span>Comment</span>
+        <img src="~/assets/images/proj1_1.png">
+        <span>Render</span>
       </div>
       <div>
-        <img src="https://via.placeholder.com/400">
-        <span>Comment</span>
+        <img src="~/assets/images/proj1_2.png">
+        <span>Large Preview</span>
       </div>
       <div>
-        <img src="https://via.placeholder.com/400">
-        <span>Comment</span>
+        <img src="~/assets/images/proj1_3.png">
+        <span>Original look of the app</span>
       </div>
     </div>
 
     <span class="uk-text-uppercase uk-text-bold">Process</span>
     <p class="uk-width-1-2 uk-margin-large-bottom">
       I've recently started falling in love with apple's
-      ui style patterns, and there is a definitely a note of inspiration in the way items are diplayed.
+      UI style patterns, and there is a definitely a note of inspiration in the way items are diplayed.
       Though, I didn't use the stereotipically easy to use San Francisco font (rest in peace Helvetica),
-      and decided to go with Inter, a great all-rounder font (which is also plastered all over this site).
+      and decided to go with Inter, a great all-rounder font that suits the needs of this project.
       This redesign was really built in mind with the larger size of the iPhone X and it's nearly
       non-existant bezels.
     </p>
