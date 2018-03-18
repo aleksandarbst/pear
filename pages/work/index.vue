40b767ce503415404aad4112c091d39f2e7b377e
Robbin Ploeger <robbin@pietersmit.nl>
2018-03-18 21:11:29 +0100

    Add project section

--- pages/work/index.vue
+++ pages/work/index.vue
@@ -27,19 +27,51 @@
         </nuxt-link>
       </div>
     </div>
+
+    <h2 class="uk-heading-primary">Projects</h2>
+    <p class="uk-text-lead uk-margin-large-bottom">Experimental</p>
+
+    <div class="uk-grid uk-child-width-1-2 uk-margin-large-bottom">
+      <div>
+        <p class="uk-text-right">
+          Gleeful, formerly known as f.esteem was a passion project that started purely
+          thanks to inspiration  and motivation to delve into the vast world of clothing
+          fashion design.
+        </p>
+        <p class="uk-text-right">
+          The initial idea was to capture the of large brands in the streetwear culture,
+          such as ASSC or Bape, as well as higher-end fashion labels such as Balenciaga,
+          Bonobos and Acne Studios.
+        </p>
+        <p class="uk-text-right">
+          What started as a quick concept of a simple graphic for t-shirts and hoodies,
+          quickly grew into an entire branding and presentation project.
+        </p>
+      </div>
+      <div>
+        <img src="https://via.placeholder.com/600" alt="">
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
+    head () {
+      return {
+        title: 'work'
+      }
+    }
   }
 </script>
 
 <style lang="scss" scoped>
-  .work-pieces img {
+  img {
     height: 100%;
     border-radius: 9px;
     box-shadow: 0 2px 6px 0 rgba(0,0,0,.2);
+  }
+  .work-pieces img {
     transition: filter 200ms;
     filter: grayscale(100%);
 
