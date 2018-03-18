0c542d4b1b0aa34f3c3a20bceb72b7f53410c7fc
Robbin Ploeger <robbin@pietersmit.nl>
2018-03-18 12:31:11 +0100

    Add lightbox to netflix redesign page

--- pages/work/index.vue
+++ pages/work/index.vue
@@ -6,24 +6,24 @@
     <div class="uk-grid uk-grid-match work-pieces">
       <div class="uk-width-1-3">
         <nuxt-link :to="'/work/netflix-redesign'">
-          <img src="~/assets/images/proj1.png">
+          <img src="/images/proj1.png">
         </nuxt-link>
       </div>
       <div class="uk-width-2-3">
         <a href="https://medium.com/@aleksbatista/standing-out-a98b3423ce05">
-          <img src="~/assets/images/proj2.png">
+          <img src="/images/proj2.png">
         </a>
       </div>
     </div>
     <div class="uk-grid uk-grid-match work-pieces uk-margin-large-bottom">
       <div class="uk-width-2-3">
         <nuxt-link :to="'/work/broadcaster-branding'">
-          <img src="~/assets/images/proj3.png">
+          <img src="/images/proj3.png">
         </nuxt-link>
       </div>
       <div class="uk-width-1-3">
         <nuxt-link :to="'/work/business-cards'">
-          <img src="~/assets/images/proj4.png">
+          <img src="/images/proj4.png">
         </nuxt-link>
       </div>
     </div>
