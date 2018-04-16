d7212282cb0f9cea865dd730b5bc418162568a02
Aleks <sasko_derp@me.com>
2018-04-16 20:06:42 +0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -5,9 +5,11 @@
       <div class="uk-width-1-2@m uk-width-1-1@s">
         <h1>Hi, my name is Aleksander</h1>
         <div class="social-menu uk-width-1-1 uk-flex uk-flex-center">
+          <a target="_blank" rel="noopener noreffer" href="https://instagram.com/uhsoory"><i class="fab fa-instagram"></i></a>
+          <a target="_blank" rel="noopener noreffer" href="https://dribbble.com/aleksbatista"><i class="fab fa-dribbble"></i></a>
           <a target="_blank" rel="noopener noreffer" href="https://twitter.com/baffledbatista"><i class="fab fa-twitter"></i></a>
-          <a target="_blank" rel="noopener noreffer" href="https://instagram.com/uhsoory/"><i class="fab fa-instagram"></i></a>
           <a target="_blank" rel="noopener noreffer" href="https://medium.com/@aleksbatista"><i class="fab fa-medium-m"></i></a>
+          
         </div>
         <p class="sub-main" style="margin-bottom: 100px;">
           — and over the past years I have grown quite familiar with the twitch community, and the environment of streamers and viewers. Through my designs, I aim to create a closer connection between said viewers and the channel broadcasters. I believe that each channel is unique, and deserves to show off exactly how special and great it is. My goal is to create and curate viable branding for any broadcaster, no matter big or small.
